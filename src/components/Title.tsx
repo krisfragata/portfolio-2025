beaf95080f0b1db131692ac134d086f48fdd527a
Kris Fragata <79350471+krisfragata@users.noreply.github.com>
2025-10-07 14:26:12 -0400

    Feat/pp 3 create home page (#3)
    * feat(docker):create dockerfile to build image of application
    
    * feat(scripts):create scripts directory to run and build docker images
    this commit also adds node script commands to run .sh scripts
    
    * feat(tailwind):configure and use krub and kumar one fonts in tailwind
    
    * feat(package.json):import dev depenedencies
    this commit imports scss as a dev dependency
    
    * feat(scripts):add dev scripts to start and stop docker containers
    this commit creates bash scripts to help with starting and stopping docker containers
    
    * feat(components):create necessary components for homepage
    this commit creates the following components:
    Footer.tsx containing socials links
    NavSidebar.tsx containing sidebar links and navigation
    Title.tsx containing page title
    
    * feat(page):add sidebar, title and footer to home page
    this commit leverages recently created Footer, NavSidebar, and Title components
    
    * feat(layout):import project fonts and pass down to children
    this commit imports the fonts Kumar One and Krub as part of next/google package and passes down to child components
    
    * feat(css): add necessary styles to components
    this commit adds styling to Title, Footer, and NavSidebar components to align with designs in figma
    
    * feat(css):ensure that styling aligns with figma designs
    this commit ensures that stylingn is readable and aligns with figma designs
    
    * feat(home):add main text bio and div wrappers
    this commit adds main bio text to home page as well as wraps components in divs to aid in css maintainance
    
    * feat(footer):ensure that links open up to new tab
    
    * feat(home):remove links not necessary
    
    * feat(css):support smaller screen sizesfor home page (#2)
    
    this commit adds @media tag to smaller screen sizes, ensuring responsiveness
    
    Co-authored-by: Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>

---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+const Title: React.FC = () => {
+    return (
+        <div className="title">
+            Kris Fragata
+        </div>
+    )
+}
+
+export default Title
