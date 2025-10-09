22ead946c17de48ebcac46d327269b8943f92a57
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2025-10-08 20:26:01 -0400

    fix(nav sidebar)add method to handle routing to new pages

--- src/components/NavSidebar.tsx
+++ src/components/NavSidebar.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface NavSideBarProps {
@@ -5,10 +7,15 @@ interface NavSideBarProps {
 }
 
 const NavSideBar: React.FC<NavSideBarProps> = ({links}) => {
+    const router = useRouter();
+
+    const handleRouteToPage = (href: string) => {
+      router.push(href)
+    }
     return (
       <div className="sidemenu">
         { links.map((link) => (
-            <a key={link.href} href={link.href} className="sidemenu-item">
+            <a key={link.href} href={link.href} className="sidemenu-item" onClick={() => handleRouteToPage(link.href)}>
                 {link.label}
             </a>
         )) }
