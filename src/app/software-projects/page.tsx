f74f1afc78db05520db1a0ca63a119e2cfcc2a13
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2025-10-09 21:06:10 -0400

    feat(software projects):create more info modal this commit adds logic to the MoreInfoModal to display necessary data for each software project state management was added to the page.tsx of software-projects directory in order to track the info modal

--- src/app/software-projects/page.tsx
+++ src/app/software-projects/page.tsx
@@ -2,29 +2,53 @@
 import React, { MouseEvent, MouseEventHandler, useRef, useState } from "react";
 import { projects } from './project-list';
 import ImageModal from "../../components/ImageModal";
+import MoreInfoModal from "@/components/MoreInfoModal";
 
 const SoftwareProjects: React.FC = () => {
     // track the state of each project?
+    // tracks state of image modals
     const [isPortfolioImageOpen, setIsPortfolioImageOpen] = useState(false);
     const [isWebScraperImageOpen, setIsWebScraperImageOpen] = useState(false);
     const [isSvisualizeImageOpen, setIsSvisualizeImageOpen] = useState(false); 
     const [isDailyDoseImageOpen, setIsDailyDoseImageOpen] = useState(false);
 
+    // tracks state of more info modals
+    const [isPortfolioTextOpen, setIsPortfolioTextOpen] = useState(false);
+    const [isWebScraperTextOpen, setIsWebScraperTextOpen] = useState(false);
+    const [isSvisualizeTextOpen, setIsSvisualizeTextOpen] = useState(false); 
+    const [isDailyDoseTextOpen, setIsDailyDoseTextOpen] = useState(false);
 
     const renderImageModals = () => {
         return projects.map(project => {
             switch (project.id) {
                 case 'portfolio':
-                    return <ImageModal project={project} setIsModalOpen={setIsPortfolioImageOpen} isModalOpen={isPortfolioImageOpen} key={project.id} extraStyling=""/>
+                    return <ImageModal project={project} setIsModalOpen={setIsPortfolioImageOpen} isModalOpen={isPortfolioImageOpen} key={project.id} extraStyling="" handleOpenText={() => setIsPortfolioTextOpen(true)}/>
                 case 'flow_scraper':
-                    return <ImageModal project={project} setIsModalOpen={setIsWebScraperImageOpen} isModalOpen={isWebScraperImageOpen} key={project.id} extraStyling="mb-10" />
+                    return <ImageModal project={project} setIsModalOpen={setIsWebScraperImageOpen} isModalOpen={isWebScraperImageOpen} key={project.id} extraStyling="mb-10"  handleOpenText={() => setIsWebScraperTextOpen(true)}/>
                 case 'svisualize':
-                    return <ImageModal project={project} setIsModalOpen={setIsSvisualizeImageOpen} isModalOpen={isSvisualizeImageOpen} key={project.id} extraStyling="mb-30"/>
+                    return <ImageModal project={project} setIsModalOpen={setIsSvisualizeImageOpen} isModalOpen={isSvisualizeImageOpen} key={project.id} extraStyling="mb-30" handleOpenText={() => setIsSvisualizeTextOpen(true)}/>
                 case 'daily_dose':
-                    return <ImageModal project={project} setIsModalOpen={setIsDailyDoseImageOpen} isModalOpen={isDailyDoseImageOpen} key={project.id} extraStyling=""/>
+                    return <ImageModal project={project} setIsModalOpen={setIsDailyDoseImageOpen} isModalOpen={isDailyDoseImageOpen} key={project.id} extraStyling="" handleOpenText={() => setIsDailyDoseTextOpen(true)}/>
             }
         })
     }
+
+    const renderInfoModals = () => {
+        return projects.map(project => {
+            switch (project.id) {
+                case 'portfolio':
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsPortfolioTextOpen} isModalOpen={isPortfolioTextOpen} key={project.id} extraStyling=""/>
+                case 'flow_scraper':
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsWebScraperTextOpen} isModalOpen={isWebScraperTextOpen} key={project.id} extraStyling="" />
+                case 'svisualize':
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsSvisualizeTextOpen} isModalOpen={isSvisualizeTextOpen} key={project.id} extraStyling=""/>
+                case 'daily_dose':
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsDailyDoseTextOpen} isModalOpen={isDailyDoseTextOpen} key={project.id} extraStyling=""/>
+            }
+        })
+    }
+
+
     
     const list = projects.map((project, index) => {
         switch (project.id) {
@@ -45,6 +69,7 @@ const SoftwareProjects: React.FC = () => {
         <div className="left-main-text">
             <ol className="software-project-list">{list}</ol>
             {renderImageModals()}
+            {renderInfoModals()}
         </div>
     );
 }
