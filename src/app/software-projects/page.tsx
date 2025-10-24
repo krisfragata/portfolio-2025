9d0513ed6ca1fea02f0b4cd287d5558c6576111b
Kristinne Fragata <krisfragata@mac.lan>
2025-10-24 17:06:47 -0400

    feat(software projects):ensure that text info opened is sent to front at all times this commit adds logic to ensure that info modals opened by the 'more info..' button are sent to the front. This is done by dynamically adjusting the z-index of each modal when clicked. new methods were added to achieve this: - state has been instantiated to track and set the 'z-index' of each info modal - sendInfoToFront loops through all the z-index state setters and sets its value to either '' or 'z-100' depending on if it is recently clicked.

--- src/app/software-projects/page.tsx
+++ src/app/software-projects/page.tsx
@@ -18,17 +18,59 @@ const SoftwareProjects: React.FC = () => {
     const [isSvisualizeTextOpen, setIsSvisualizeTextOpen] = useState(false); 
     const [isDailyDoseTextOpen, setIsDailyDoseTextOpen] = useState(false);
 
+    // tracks state of info-modal z-indices
+    const [portfolioTextZIndez, setPortfolioTextZIndex] = useState<string>('');
+    const [webScraperZIndex, setWebScraperZIndex] = useState<string>('');
+    const [sviualizeZIndex, setSvisualizeZIndex] = useState<string>('');
+    const [dailyDoseZIndex, setDailyDoseZIndex] = useState<string>('');
+
+    interface ModalSetter {
+        type: String,
+        setter: Function,
+    }
+
+    const sendInfoToFront = (project: string) => {
+        // array holding all info-modal state setters
+        const modalSetters: ModalSetter[] = [
+            {
+                type: 'portfolio',
+                setter: setPortfolioTextZIndex,
+            },
+            {
+                type: 'flow_scraper',
+                setter: setWebScraperZIndex,
+            },
+            {
+                type: 'svisualize',
+                setter: setSvisualizeZIndex,
+            },
+            {
+                type: 'daily_dose',
+                setter: setDailyDoseZIndex,
+            }
+        ];
+
+        // method that loops through modalSetters and ensures that passed in project is sent to front and all others are null.
+        modalSetters.forEach((modal) => {
+            if (modal.type === project) {
+                modal.setter('z-100');
+            } else {
+                modal.setter(null);
+            }
+        });
+    }
+
     const renderImageModals = () => {
         return projects.map(project => {
             switch (project.id) {
                 case 'portfolio':
-                    return <ImageModal project={project} setIsModalOpen={setIsPortfolioImageOpen} isModalOpen={isPortfolioImageOpen} key={project.id} extraStyling="" handleOpenText={() => setIsPortfolioTextOpen(true)}/>
+                    return <ImageModal project={project} setIsModalOpen={setIsPortfolioImageOpen} isModalOpen={isPortfolioImageOpen} key={project.id} extraStyling={``} handleTextToggle={() => { sendInfoToFront('portfolio'); const isTextOpen = isPortfolioTextOpen; setIsPortfolioTextOpen(!isTextOpen)}}/>
                 case 'flow_scraper':
-                    return <ImageModal project={project} setIsModalOpen={setIsWebScraperImageOpen} isModalOpen={isWebScraperImageOpen} key={project.id} extraStyling="mb-10"  handleOpenText={() => setIsWebScraperTextOpen(true)}/>
+                    return <ImageModal project={project} setIsModalOpen={setIsWebScraperImageOpen} isModalOpen={isWebScraperImageOpen} key={project.id} extraStyling={`mb-10`}  handleTextToggle={() => { sendInfoToFront('flow_scraper'); const isTextOpen = isWebScraperTextOpen; setIsWebScraperTextOpen(!isTextOpen)}}/>
                 case 'svisualize':
-                    return <ImageModal project={project} setIsModalOpen={setIsSvisualizeImageOpen} isModalOpen={isSvisualizeImageOpen} key={project.id} extraStyling="mb-30" handleOpenText={() => setIsSvisualizeTextOpen(true)}/>
+                    return <ImageModal project={project} setIsModalOpen={setIsSvisualizeImageOpen} isModalOpen={isSvisualizeImageOpen} key={project.id} extraStyling={`mb-30`} handleTextToggle={() => { sendInfoToFront('svisualize'); const isTextOpen = isSvisualizeTextOpen; setIsSvisualizeTextOpen(!isTextOpen)}}/>
                 case 'daily_dose':
-                    return <ImageModal project={project} setIsModalOpen={setIsDailyDoseImageOpen} isModalOpen={isDailyDoseImageOpen} key={project.id} extraStyling="" handleOpenText={() => setIsDailyDoseTextOpen(true)}/>
+                    return <ImageModal project={project} setIsModalOpen={setIsDailyDoseImageOpen} isModalOpen={isDailyDoseImageOpen} key={project.id} extraStyling={``} handleTextToggle={() => { sendInfoToFront('daily_dose'); const isTextOpen = isDailyDoseTextOpen; setIsDailyDoseTextOpen(!isTextOpen)}}/>
             }
         })
     }
@@ -37,13 +79,13 @@ const SoftwareProjects: React.FC = () => {
         return projects.map(project => {
             switch (project.id) {
                 case 'portfolio':
-                    return <MoreInfoModal project={project} setIsModalOpen={setIsPortfolioTextOpen} isModalOpen={isPortfolioTextOpen} key={project.id} extraStyling=""/>
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsPortfolioTextOpen} isModalOpen={isPortfolioTextOpen} key={project.id} extraStyling={`${portfolioTextZIndez}`}/>
                 case 'flow_scraper':
-                    return <MoreInfoModal project={project} setIsModalOpen={setIsWebScraperTextOpen} isModalOpen={isWebScraperTextOpen} key={project.id} extraStyling="" />
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsWebScraperTextOpen} isModalOpen={isWebScraperTextOpen} key={project.id} extraStyling={`${webScraperZIndex}`} />
                 case 'svisualize':
-                    return <MoreInfoModal project={project} setIsModalOpen={setIsSvisualizeTextOpen} isModalOpen={isSvisualizeTextOpen} key={project.id} extraStyling=""/>
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsSvisualizeTextOpen} isModalOpen={isSvisualizeTextOpen} key={project.id} extraStyling={`${setSvisualizeZIndex}`}/>
                 case 'daily_dose':
-                    return <MoreInfoModal project={project} setIsModalOpen={setIsDailyDoseTextOpen} isModalOpen={isDailyDoseTextOpen} key={project.id} extraStyling=""/>
+                    return <MoreInfoModal project={project} setIsModalOpen={setIsDailyDoseTextOpen} isModalOpen={isDailyDoseTextOpen} key={project.id} extraStyling={`${setSvisualizeZIndex}`}/>
             }
         })
     }
