059e67558c8ba95455b57b156a61b199705e1059
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2025-10-25 21:12:08 -0400

    fix(image modal):address compile errors in path component rename css properties in path component to match with next's expectations. Such as, strokeLinecap, strokeLinejoin and strokeWidth instead of stroke-linecap, stroke-linejoin and stroke-width

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -52,7 +52,7 @@ const ImageModal: React.FC<ImageModalProps> = ({project, setIsModalOpen, isModal
                 {/* only show arrows if there is more than one image */}
                 {project.imageSources.length > 1 && <div className="arrow arrow-left">
                     <svg className="w-6 h-6 text-primary arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-                        <path stroke="#D22727" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
+                        <path stroke="#D22727" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
                     </svg>
                 </div>}
                 <div className="software-project-image">
@@ -61,7 +61,7 @@ const ImageModal: React.FC<ImageModalProps> = ({project, setIsModalOpen, isModal
                 {/* only show arrows if there is more than one image */}
                 {project.imageSources.length > 1 && <div className="arrow arrow-right" onClick={handleBackImage}>
                     <svg className="w-6 h-6 text-primary-red arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-                        <path stroke="#D22727" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
+                        <path stroke="#D22727" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
                     </svg>
                 </div>}
             </div>
