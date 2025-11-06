00ac4efa5ac8adbc4822b8cf120c497f0a9bf0c7
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2025-11-06 15:40:11 -0500

    feat(experiences):add logic to allow info modals to open this commit adjusts the 'more-info' components to have onclick feature allowing info modals to open

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -2,11 +2,14 @@
 import React, { JSX, useState } from "react";
 import { Work, works } from "./work_list";
 import { Education, educationList } from "./education_list";
+import EducationModal from "@/components/EducationModal";
 
 const Experiences: React.FC = () => {
     // state trackers
-    const [isEducationExpanded, setIsEducationExpanded] = useState<boolean>(false);
-    const [isWorkExpanded, setIsWorkExpanded] = useState<boolean>(false);
+    const [isEducationExpanded, setIsEducationExpanded] = useState<boolean>(true);
+    const [isEducationModalOpen, setIsEducationModalOpen] = useState<boolean>(false);
+    const [isWorkExpanded, setIsWorkExpanded] = useState<boolean>(true);
+    const [isWorkModalOpen, setIsWorkModalOpen] = useState<boolean>(false);
 
     /**
      * 
@@ -15,7 +18,15 @@ const Experiences: React.FC = () => {
     const renderEducationList = () => {
       return  <div className="experiences-list-container">
         <ul className="">{renderEducationListItems}</ul>
-        <div className="more-button">(more info)</div>
+        <div 
+            className="more-button"
+            onClick={() => {
+                const isOpen = isEducationModalOpen;
+                setIsEducationModalOpen(!isOpen);
+            }}
+        >
+            (more info)
+        </div>
       </div>
     }
 
@@ -30,6 +41,18 @@ const Experiences: React.FC = () => {
            value={edu.id}
         ><span className="text-primary list-numbers-container">{`0${index}`}</span>{`${edu.schoolName}`}</li>
     });
+
+    /**
+     * @returns {JSX} - education modal component
+     */
+    const renderEducationInfoModal = () => {
+        return <EducationModal 
+            educationList={educationList}
+            isModalOpen={isEducationModalOpen}
+            setIsModalOpen={setIsEducationModalOpen}
+            extraStyling=""
+        />
+    }
     
     /**
      * @returns {JSX} - component containing work list
@@ -37,7 +60,15 @@ const Experiences: React.FC = () => {
     const renderWorkList = () => {
         return <div className="experiences-list-container">
         <ul className="">{renderWorkListItems}</ul>
-        <div className="more-button">(more info)</div>
+        <div 
+            className="more-button"
+            onClick={() => {
+                const isOpen = isWorkModalOpen;
+                setIsWorkModalOpen(!isOpen);
+            }}
+        >
+            (more info)
+        </div>
       </div>
     }
     /**
@@ -50,7 +81,7 @@ const Experiences: React.FC = () => {
             key={index}
             value={work.id}
         ><span className="text-primary list-numbers-container">{`0${index}`}</span>{`${work.companyName}`}</li>
-    })
+    });
     return (
         <div className="left-main-text">
             <ul className="edu-work-icons">
@@ -75,6 +106,7 @@ const Experiences: React.FC = () => {
                     isWorkExpanded && renderWorkList()
                 }
             </ul>
+            {renderEducationInfoModal()}
         </div>
     );
 };
