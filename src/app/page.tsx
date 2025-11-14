32ec6c451fa1533baf41e81f662b95cf3783d7b4
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2025-11-14 14:49:36 -0500

    feat(home):add logic to display hover blurb this commit adds logic to render a blurb component that displays on hover of 'manila, philippines' this commit also adds an event listener to window size to ensure that hover feature is compatible with mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,69 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [isBlurbOpen, setIsBlurbOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+  const [isSoftwareBlurbOpen, setIsSoftwareBlurbOpen] = useState(false);
+
+  useEffect(() => {
+    if (!windowWidth) {
+      setWindowWidth(window.innerWidth);
+    }
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+  }, []);
+
+  /**
+   * @description renders blurb about Manila. Presence also turns on canvas feature
+   * @returns {JSX} - blurb component
+   */
+  const renderBlurb = () => {
+    
+    return (
+      <div className="blurb animate-pulse">
+        <p className="blurb-text">I do NOT live in Manila. But I miss the Philippines so much that this is more of a manifestation :)</p>
+      </div>
+    )
+  }
+
+  /**
+   * @description renders blurb about software blurb. 
+   * @returns {JSX} - blurb component
+   */
+  const renderSoftwareBlurb = () => {
+    return (
+      <div className="blurb">
+        <p className="blurb-text">something cool will happen if you click this... one day</p>
+      </div>
+    )
+  }
+
   return (
     <div className="left-main-text">
-      I’m a <span className="text-primary">software engineer!</span> and <span className="text-primary">artist!</span> based in Boston, Massachusetts and Manila, Philippines. 
+      I’m a <span className="text-primary hover:cursor-pointer hover:font-semibold">software engineer!</span> and <span className="text-primary hover:cursor-pointer hover:font-semibold">artist!</span> based in Boston, Massachusetts and <span 
+        className="highlight" 
+        onMouseOver={() => {
+          if (windowWidth < 768) return;
+          setIsBlurbOpen(true);
+        }}
+        onMouseOut={() => {
+          if (windowWidth < 768) return;
+          setIsBlurbOpen(false)
+        }}
+        onClick={() => {
+          const isOpen = isBlurbOpen;
+          setIsBlurbOpen(!isOpen);
+        }}
+      >Manila, Philippines</span>.
+      {isBlurbOpen && renderBlurb()}
+      {isSoftwareBlurbOpen && renderSoftwareBlurb()}
     </div>
   );
 }
